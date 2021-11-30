b72f17bb2529be4923bd6f53d4be0b66fb56296e
nguyen oni <an64440@gmail.com>
2021-11-30 11:31:40 +0700

    create models

--- baogia/quote/models.py
+++ baogia/quote/models.py
@@ -23,24 +23,61 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "slug": self.price,
+            "price": self.price,
+            "quantity": self.quantity
+        }
+# Dung tích
+class Volume(models.Model):
+    name = models.CharField(max_length=50, default= "", verbose_name="Tên dung tích")
+    is_activate = models.BooleanField(default=True, verbose_name="Trạng thái")
+    product = models.ManyToManyField(Product, related_name="product")
 
-class Service_Costs(models.Model):
-    name = models.CharField(max_length=255, default="", verbose_name="Tên gói DV đính kèm")
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="product")
-    price_material = models.FloatField(default=0, verbose_name="Giá nguyên liệu")
-    price_pack = models.FloatField(default=0, verbose_name="Giá đóng gói")
-    packaging = models.FloatField(default=0, verbose_name="Bao bì")
-    box = models.FloatField(default=0, verbose_name="Hộp")
-    label_stamp = models.FloatField(default=0, verbose_name="Tem nhãn, phd, hdd")
-    time_wait_product = models.TextField(default="", verbose_name="Thời gian chờ hàng")
-    note = models.TextField(default="", verbose_name="Ghi chú")
-    create_at = models.DateTimeField(auto_created=True, verbose_name="Ngày tạo")
-    update_at = models.DateTimeField(auto_now=True, verbose_name="Ngày cập nhật")
+    def __str__(self):
+        return self.name
+
+# Nguyên liệu
+class Material(models.Model):
+    name = models.CharField(max_length=255, default="", verbose_name="Tên nguyên liệu")
+    price = models.FloatField(default=0, verbose_name="Giá nguyên liệu")
+    product = models.ManyToManyField(Product, related_name="product", verbose_name="Sản phẩm") #nguyên liệu của sản phẩm nào
+    volume = models.ManyToManyField(Volume, related_name="volume", verbose_name="Dung tích")
     is_activate = models.BooleanField(default=True, verbose_name="Trạng thái")
 
     def __str__(self):
         return self.name
 
-class ListQuotes(models.Model):
-    # user, product, service, amount, sum
-    product = models.ManyToManyField(Product,)
+# Bao bì
+class Packaging(models.Model):
+    name = models.CharField(max_length=255, default="", verbose_name="Tên bao bì")
+    price = models.FloatField(default=0, verbose_name="Giá bao bì")
+    product = models.ManyToManyField(Product, related_name="product", verbose_name="Sản phẩm") #nguyên liệu của sản phẩm nào
+    volume = models.ManyToManyField(Volume, related_name="volume", verbose_name="Dung tích")
+    is_activate = models.BooleanField(default=True, verbose_name="Trạng thái")
+
+    def __str__(self):
+        return self.name
+# Hộp đựng sản phẩm
+class Box_Product(models.Model):
+    name = models.CharField(max_length=255, default="", verbose_name="Tên hộp đựng sản phẩm")
+    price = models.FloatField(default=0, verbose_name="Giá hộp đựng sản phẩm")
+    product = models.ManyToManyField(Product, related_name="product", verbose_name="Sản phẩm") #nguyên liệu của sản phẩm nào
+    volume = models.ManyToManyField(Volume, related_name="volume", verbose_name="Dung tích")
+    is_activate = models.BooleanField(default=True, verbose_name="Trạng thái")
+
+    def __str__(self):
+        return self.name
+
+# Thùng caton
+class Carton_Box(models.Model):
+    name = models.CharField(max_length=255, default="", verbose_name="Tên thùng caton")
+    price = models.FloatField(default=0, verbose_name="Giá thùng caton")
+    product = models.ManyToManyField(Product, related_name="product", verbose_name="Sản phẩm") #nguyên liệu của sản phẩm nào
+    volume = models.ManyToManyField(Volume, related_name="volume", verbose_name="Dung tích")
+    is_activate = models.BooleanField(default=True, verbose_name="Trạng thái")
+
+    def __str__(self):
+        return self.name
