6fe77f118d58268b6fd31a771c3db9da18dc0163
nguyenoni <32008549+nguyenoni@users.noreply.github.com>
2021-12-04 18:00:54 +0700

    Update urls.py

--- baogia/quote/urls.py
+++ baogia/quote/urls.py
@@ -1,17 +1,20 @@
 from django.urls import path
 from django.urls.conf import path, re_path
-from .views import list_category, detail_category, load_packing_worker, load_volume_product, load_material, load_packaging_level1,load_packaging_level2, load_stamp
-
+from .views import list_category
+from . import views
 urlpatterns = [
     path('', list_category.as_view(), name='list_category'),
-    path('<slug:slug>', detail_category, name='detail_category'),
+    path('<slug:slug>', views.detail_category, name='detail_category'),
     # API
-    path('api/load-volume-product/', load_volume_product, name="load_volume_product"),
-    path('api/load-material/', load_material, name="load_material"),
-    path('api/load-packaging-level1/', load_packaging_level1, name="load_packaging_level1"),
-    path('api/load-packaging-level2/', load_packaging_level2, name="load_packaging_level2"),
-    path('api/load-stamp/', load_stamp, name="load_stamp"),
-    path('api/load-packing-worker/', load_packing_worker, name="load_stamp"),
+    path('api/load-volume-product/', views.load_volume_product, name="load_volume_product"),
+    path('api/load-material/', views.load_material, name="load_material"),
+    path('api/load-packaging-level1/', views.load_packaging_level1, name="load_packaging_level1"),
+    path('api/load-packaging-level2/', views.load_packaging_level2, name="load_packaging_level2"),
+    path('api/load-stamp/', views.load_stamp, name="load_stamp"),
+    path('api/load-packing-worker/', views.load_packing_worker, name="load_stamp"),
+    path('api/load-announced/', views.load_announced, name="load_announced"),
+    path('api/load-feeship/', views.load_feeship, name="load_feeship"),
+
 
 
 
