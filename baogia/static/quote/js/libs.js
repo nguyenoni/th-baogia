6bec5a74e0a2ae88af8bdb10bea5cf9716cdc4b9
nguyenoni <32008549+nguyenoni@users.noreply.github.com>
2021-12-04 17:58:40 +0700

    Update libs.js

--- baogia/static/quote/js/libs.js
+++ baogia/static/quote/js/libs.js
@@ -1,3 +1,15 @@
 function get_csrfmiddlewaretoken() {
 	return $(".no-s").attr('val');
 }
+
+function show_mess(mess){
+    toastr.success(mess);
+}
+
+const MATERIAL = "MATERIAL";
+const PACKAGINGLEVEL1 = "PACKAGINGLEVEL1";
+const PACKAGINGLEVEL2 = "PACKAGINGLEVEL2";
+const STAMP = "STAMP";
+const PACKINGWORKER = "PACKINGWORKER";
+const ANNOUNCED = "ANNOUNCED";
+const FEESHIP = "FEESHIP";
